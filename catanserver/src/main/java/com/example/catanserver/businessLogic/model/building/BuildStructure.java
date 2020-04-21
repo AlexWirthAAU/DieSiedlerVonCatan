ab896f030c885bb88bb737c44be884493058c722
Klemens Vospernik <klemens.vospernik@gmx.at>
2020-04-21 23:34:26 +0200

    BuildStructure Interface and BuildSettlement Class inital commit

---
+++ catanserver/src/main/java/com/example/catanserver/businessLogic/model/building/BuildStructure.java
@@ -0,0 +1,13 @@
+package com.example.catanserver.businessLogic.model.building;
+
+import com.example.catanserver.businessLogic.model.PlayerImpl;
+import com.example.catanserver.businessLogic.model.gameboard.Knot;
+
+
+public interface BuildStructure { //todo: should allow generic input: Knot, Edge (for method selectBuildingSite(<T>)
+    //instantiate PlayerImpl and Knot as private variables
+    boolean checkResources(); //prerequisite to being able to buy a structure
+    void selectBuildingSite(); //prerequisite to being allowed to build a structure at a certain site
+    void updateGameboard();
+    void updatePlayerInventory();
+}
