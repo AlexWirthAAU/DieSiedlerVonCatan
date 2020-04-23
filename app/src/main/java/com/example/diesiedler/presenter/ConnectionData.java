4ee78d3f3e4926c7ee888d7f14f58fcd2649128f
Klemens Vospernik <klemens.vospernik@gmx.at>
2020-04-23 07:19:04 +0200

    minor change to ConnectionData (addresses sonarcloud issue)

--- app/src/main/java/com/example/diesiedler/presenter/ConnectionData.java
+++ app/src/main/java/com/example/diesiedler/presenter/ConnectionData.java
@@ -4,9 +4,15 @@ class ConnectionData {
 
     static final String HOST = "193.81.249.102";
     static final int PORT = 2020;
-
+    /* did not work:
     public ConnectionData() {
         //added empty constructor so the implicit one is not used because sonarcloud said that this is an issue
         //sonarcloud message: "Utility classes should not have public constructors"
+    } */
+
+    private ConnectionData() {
+        //as proposed from sonarcloud:
+        throw new IllegalStateException("Utility class");
     }
+
 }
