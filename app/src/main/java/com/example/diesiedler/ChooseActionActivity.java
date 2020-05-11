b89aeff873f8ce565ba0e2270fc9a39ef8a2ad5a
AlexWirthAAU <AlexWirthAAU@users.noreply.github.com>
2020-05-11 13:16:02 +0200

    adapted build gradle

--- app/src/main/java/com/example/diesiedler/ChooseActionActivity.java
+++ app/src/main/java/com/example/diesiedler/ChooseActionActivity.java
@@ -4,10 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.nio.BufferUnderflowException;
 
 
 public class ChooseActionActivity extends AppCompatActivity implements View.OnClickListener {
